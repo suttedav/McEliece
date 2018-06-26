ab5225c33fb9b66ce4f19f5460c2286ecc01583e
David Sutter <davi.sutter@gmail.com>
2018-06-26 22:50:50 +0200

    added some code to read in a text file which is then converted to a binary string and then converted back to a text string

--- Test.py
+++ Test.py
@@ -68,3 +68,34 @@ print(c_hat)
 
 print("Test code completed :-)" )
 print("test GitHub")
+
+
+# ========== read in txt file
+f = open('test_input.txt','r')
+message = f.read()
+print(message)
+f.close()
+
+
+import binascii
+
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return int2bytes(n).decode(encoding, errors)
+
+def int2bytes(i):
+    hex_string = '%x' % i
+    n = len(hex_string)
+    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+
+
+message_bin=text_to_bits(message)
+print(message_bin)
+print("length of binary string is", len(message_bin))
+message_normal=text_from_bits(message_bin)
+print(message_normal)
+
